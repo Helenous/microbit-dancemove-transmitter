9b1b72caf473cd9557fbfe030364b0c286f03d5f
Helenous <heleneviro@hotmail.com>
2021-04-04 11:10:01 +0100

    changemain1

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-def on_button_pressed_a():
-    global sendData
-    sendData = not (sendData)
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-previousMillis = 0
-currentMillis = 0
-sendData = False
-radio.set_group(1)
-strVar = "Hello World!"
-duration = 1000
-sendData = False
-
 def on_forever():
-    global currentMillis, previousMillis
-    while sendData:
-        radio.send_string(strVar)
-        currentMillis = control.millis()
-        led.plot_brightness(0, 0, 255)
-        if currentMillis - previousMillis >= duration:
-            led.plot_brightness(0, 0, 0)
-            previousMillis = currentMillis
-        basic.pause(100)
-    led.plot_brightness(0, 0, 0)
+    if images.icon_image(IconNames.HEART) == images.arrow_image(ArrowNames.WEST):
+        kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
+            kitronik_motor_driver.MotorDirection.FORWARD,
+            81)
+        kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR2,
+            kitronik_motor_driver.MotorDirection.FORWARD,
+            100)
+        basic.pause(500)
 basic.forever(on_forever)
+
